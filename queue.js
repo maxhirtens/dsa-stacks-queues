fcdf95c2e550ec9f77767932ddd912c2346700f7
Max Hirtenstein <maxhirtens@gmail.com>
2023-06-06 12:44:33 -0700

    done

--- queue.js
+++ queue.js
@@ -20,26 +20,52 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    let node = new Node(val);
 
+    if (!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+      this.last.next = node;
+      this.last = node;
+    }
+
+    this.size++;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
-
+    if (!this.first) {
+      throw new Error("nothing there.");
+    }
+    let temp = this.first;
+    if (this.first == this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.val;
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    if (!this.first) {
+      throw new Error("nothing there.");
+    }
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    if (!this.size) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
